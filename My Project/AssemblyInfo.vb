adc77c4b0bc00ceedd5dd0f1016f0ffabb2c8d05
argynnini <user@KATAOKA-PC>
2023-06-22 01:25:02 +0900

    first branch

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します
+
+<Assembly: AssemblyTitle("OfficeAgent")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("K.B.T.C.")>
+<Assembly: AssemblyProduct("OfficeAgent")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+<Assembly: Guid("e7933670-91a3-4075-aa54-858b4f5a2b7b")>
+
+' アセンブリのバージョン情報は次の 4 つの値で構成されています:
+'
+'      メジャー バージョン
+'      マイナー バージョン
+'      ビルド番号
+'      Revision
+'
+' すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.2.0.0")>
+<Assembly: AssemblyFileVersion("1.2.0.0")>
